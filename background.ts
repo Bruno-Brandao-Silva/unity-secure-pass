24efe644adee42b50e3082dbad7550318d9c33e4
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-06-13 14:32:33 -0300

    Added Persistent Memory and Fixed Typo

--- background.ts
+++ background.ts
@@ -1,9 +1,30 @@
 let onOffInput: boolean = true;
+
+
+chrome.runtime.onStartup.addListener(function () {
+	chrome.storage.local.get('onOffInput', function (result) {
+		var data = result.onOffInput;
+		console.log('Dados recuperados:', data);
+		if (data===undefined) {
+			chrome.storage.local.set({ 'onOffInput': true }, function () {
+				console.log('Dados salvos com sucesso!');
+			});
+			onOffInput = true;
+		} else {
+			onOffInput = data;
+		}
+	});
+});
+
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.status === 'init') {
 		sendResponse({ checkboxState: onOffInput });
 	} else if (request.status === 'change') {
 		onOffInput = request.checkboxState;
+		chrome.storage.local.set({ 'onOffInput': onOffInput }, function () {
+			console.log('Dados salvos com sucesso!');
+		});
 		sendResponse({})
 	}
 });
